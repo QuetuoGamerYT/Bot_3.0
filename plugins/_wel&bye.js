56d2a2dd222b04c2938c2db8bb58cf12d4b7c6f3
emere <emerech789@gmail.com>
2022-11-14 09:31:57 -0500

    Actualizacion 3.0

---
+++ plugins/_wel&bye.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn, text, usedPrefix, command, customPrefix }) => {
+let stikerwelgc = "./src/welgc.webp"
+let stikerbyegc = "./src/byegc.webp"
+if (command == 'welcomegc') {
+conn.sendFile(m.chat, stikerwelgc, 'sticker.webp', null, m, false, { 
+contextInfo: { externalAdReply: { title: 'ᴛʜᴇ ᴍʏsᴛɪᴄ - ʙᴏᴛ', body: '©𝙌𝙪𝙚𝙩𝙪𝙤', sourceUrl: `https://github.com/QuetuoGamerYT/Bot_uwu.git`, thumbnail: imagen1}}})  
+}
+if (command == 'byegc') {
+conn.sendFile(m.chat, stikerbyegc, 'sticker.webp', null, m, false, { 
+contextInfo: { externalAdReply: { title: 'ᴛʜᴇ ᴍʏsᴛɪᴄ - ʙᴏᴛ', body: '©𝙌𝙪𝙚𝙩𝙪𝙤', sourceUrl: `https://github.com/QuetuoGamerYT/Bot_uwu.git`, thumbnail: imagen1}}})
+}}
+handler.command = ['welcomegc', 'byegc']
+export default handler
+function pickRandom(list) {
+return list[Math.floor(list.length * Math.random())]}
